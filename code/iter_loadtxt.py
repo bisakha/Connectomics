d1d1d35048f0afbf020fc70e0fe0dbcb529121bb
bisakha <rbisakha@gmail.com>
2014-04-09 16:16:21 -0400

    Create iter_loadtxt.py

---
+++ code/iter_loadtxt.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+##Code from http://stackoverflow.com/questions/
+##8956832/python-out-of-memory-on-large-csv-file-numpy
+
+def iter_loadtxt(filename, delimiter=',', skiprows=0, dtype=float):
+    def iter_func():
+        with open(filename, 'r') as infile:
+            for _ in range(skiprows):
+                next(infile)
+            for line in infile:
+                line = line.rstrip().split(delimiter)
+                for item in line:
+                    yield dtype(item)
+        iter_loadtxt.rowlength = len(line)
+
+    data = np.fromiter(iter_func(), dtype=dtype)
+    data = data.reshape((-1, iter_loadtxt.rowlength))
+    return data
