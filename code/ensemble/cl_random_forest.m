57bd798d8f73def842d567aaa75d2ff4d3b96fc7
Bisakha Ray <rbisakha@gmail.com>
2017-05-09 12:34:51 -0400

    Create cl_random_forest.m

---
+++ code/ensemble/cl_random_forest.m
@@ -0,0 +1,8 @@
+function [prediction_discrete, variable_importance] = cl_random _forest(data, target, TRAIN_indx, TEST_indx)
+% model = svm_train(target(TRAIN_indx),data(TRAIN_indx,:), options);
+    model = TreeBagger(100,data(TRAIN_indx,:), target(TRAIN_indx), 'Method', 'classification','OOBVarImp','On');
+    variable_importance = model.OOBPermutedVarDeltaError; 
+   % model = NaiveBayes.fit(data(TRAIN_indx,:), target(TRAIN_indx));
+   prediction_discrete = model.predict(data(TEST_indx,:));
+   
+   % [prediction_discrete, ~, prediction_continuous] = svm_predict(nan(length(TEST_indx),1),data(TEST_indx,:), model)
